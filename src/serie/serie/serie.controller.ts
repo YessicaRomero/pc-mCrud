030cc74e67a88e610aecc9fd697eb76ba9cddb7e
Yessica <yessilpd91@outlook.com>
2024-03-30 19:06:50 -0300

    atajando errores con try-catch

--- src/serie/serie/serie.controller.ts
+++ src/serie/serie/serie.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpStatus, Param, Post, Put, Res, HttpCode } from '@nestjs/common';
 import { SerieService } from './serie.service';
 import { iSerie } from './serie.interface';
 import { get } from 'http';
+import { response } from 'express';
 
 @Controller('series')
 export class SerieController {
@@ -10,21 +11,39 @@ public constructor(private readonly SerieService: SerieService){}
 getseries(): Promise<iSerie[]>{
     return this.SerieService.getSeries();
 }
-@Get()
-getSerieById(@Param('id')id:string) : Promise<iSerie>{
-    return this.SerieService.getSerieById(id)
+@Get(':id')
+async getSerieById(@Res() response, @Param('id')id:string) : Promise<any>{
+    try{
+        const responseFromService = await this.SerieService.getSerieById(id)
+        if(Object.keys(responseFromService).length){
+            return response.status(HttpStatus.OK).json(responseFromService);
+        }else {
+              return response
+              .status(HttpStatus.NOT_FOUND)
+              .json({ error: 'La serie no existe'});
+        }
+              
+    } catch (error){
+        return response
+        .status(HttpStatus.NOT_FOUND)
+        .json({ error: 'El id no existe'})
+    }
+    
 }
-@Post()
-create(@Body() serie:iSerie) {
+
+
+
+        @Post()
+    create(@Body() serie:iSerie) {
     return this.SerieService.addSerie(serie)
 }
-@Delete(':id')
+    @Delete(':id')
 deleteByIdSerie(@Param('id') id:string){
     return this.SerieService.deleteByIdSerie(id);
 
 }
-@Put(':id')
-updateSerieById(
+    @Put(':id')
+      updateSerieById(
     @Param('id') id:string,
     @Body() body: iSerie,
 ): Promise<void> {
